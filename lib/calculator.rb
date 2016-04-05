811c530cc837f523eada733fe87917f3a0dc9dae
Rebecca Piper <piper.rebecca93@gmail.com>
2016-04-05 11:26:43 +0100

    Passed second test. Returns 1 if string = '1'

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,6 @@
 class Calculator
   def addInt(string)
+    return 1 if string == "1"
     0
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,11 @@ describe Calculator do
     it "returns zero when given an empty string" do
       expect(calculator.addInt("")).to eq 0
     end
+
+    context "given one number" do
+      it "returns 1 when given the string '1'" do
+        expect(calculator.addInt("1")).to eq 1
+      end
+    end
   end
 end
