0ff2442a66044e33a687f613cb53b9a6db96844c
Rebecca Piper <piper.rebecca93@gmail.com>
2016-04-05 11:48:36 +0100

    Wrote tests for two numbers in a string. Made tests pass.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,11 +1,10 @@
 class Calculator
   def addInt(string)
-    return 0 if empty?(string)
-    string.to_i
-  end
-
-  private
-  def empty?(string)
-    string == ""
+    return 0 if string.empty?
+    array = string.split(",")
+    array.map! do |x|
+      x.to_i
+    end
+    array.reduce(:+)
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -16,5 +16,15 @@ describe Calculator do
         expect(calculator.addInt("5")).to eq 5
       end
     end
+
+    context "given two numbers" do
+      it "returns 3 when given the string '1,2'" do
+        expect(calculator.addInt("1,2")).to eq 3
+      end
+
+      it "returns the sum of two numbers" do
+        expect(calculator.addInt("3,5")).to eq 8
+      end
+    end
   end
 end
