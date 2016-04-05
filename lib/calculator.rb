fb39d9096b00d0ae7beb206075eec20dbc5c7186
Rebecca Piper <piper.rebecca93@gmail.com>
2016-04-05 11:56:22 +0100

    Refactored code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,13 @@
 class Calculator
   def addInt(string)
     return 0 if string.empty?
+    string_to_sum(string)
+  end
+
+  private
+  def string_to_sumgit (string)
     array = string.split(",")
-    array.map! do |x|
-      x.to_i
-    end
+    array.map! { |x| x.to_i }
     array.reduce(:+)
   end
 end
